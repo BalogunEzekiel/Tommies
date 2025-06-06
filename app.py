12a5292a43a3d38f760f400bec80e0d2a07726b2
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-06 22:29:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -620,6 +620,7 @@ def main():
     # Sidebar Branding
     st.sidebar.title("About Perfectfit 👗🧵")
     st.sidebar.info("Perfectfit is your one-stop fashion store offering premium styles at unbeatable prices.")
+    st.sidebar.markdown("***.....fast and reliable.***")
 
 if __name__ == "__main__":
     main()
