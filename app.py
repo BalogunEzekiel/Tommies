15857e1f8f6cc691150e8d8997ca2fea853ed47d
Ezekiel Balogun <ezekiel4true@yahoo.com>
2025-06-06 22:50:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -619,21 +619,9 @@ if __name__ == "__main__":
 def main():
     # Sidebar Branding
     st.sidebar.title("About Perfectfit 👗🧵")
-    st.sidebar.markdown(
-        """
-        <div style="
-            background-color: #28a745;  /* Bootstrap green */
-            color: white;
-            padding: 10px;
-            border-radius: 5px;
-            font-weight: bold;
-            font-style: italic;
-            ">
-            .....fast and reliable.<br><br>
-            Perfectfit is your one-stop fashion store offering premium styles at unbeatable prices.
-        </div>
-        """,
-        unsafe_allow_html=True
+    st.sidebar.info(
+    "***.....fast and reliable.***\n\n"
+    "Perfectfit is your one-stop fashion store offering premium styles at unbeatable prices."
     )
 
 if __name__ == "__main__":
